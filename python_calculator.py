44606642cf168c5492b712ae3fbbbd0f2062e7be
Soniel <tamboonsonieljay@gmail.com>
2026-05-08 21:24:07 +0800

    start all over again by importing tkinter (GUI) and creating the window

--- python_calculator.py
+++ python_calculator.py
@@ -1,23 +1,6 @@
-# Simple Calculator Program
+import tkinter as tk
 
-print("=== Simple Calculator ===")
+root = tk.Tk()
+root.title("python_calculator")
+root.geometry("400x400")
 
-number1 = float(input("Enter first number: "))
-operator = input("Enter operator (+, -, *, /): ")
-number2 = float(input("Enter second number: "))
-
-if operator == "+":
-    result = number1 + number2
-elif operator == "-":
-    result = number1 - number2
-elif operator == "*":
-    result = number1 * number2
-elif operator == "/":
-    if number2 != 0:
-        result = number1 / number2
-    else:
-        result = "Error: Cannot divide by zero"
-else:
-    result = "Invalid Operator"
-
-print("Result:", result)
