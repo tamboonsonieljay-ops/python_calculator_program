da2bde3b51a17005d9b8952b01d2d1bbfdc5c5e9
Soniel <tamboonsonieljay@gmail.com>
2026-05-08 22:47:41 +0800

    applied the class inheritance to the calculator with the parent class named calculator and the child class advanced_calculator

--- python_calculator.py
+++ python_calculator.py
@@ -1,52 +1,66 @@
 import tkinter as tk
 
+class calculator:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Python Calculator")
+        self.root.geometry("400x500")
+        self.root.resizable(0,0)
+
+        self.entry = tk.Entry(self.root, font=("Arial", 20), justify="right")
+        self.entry.pack(fill="both",padx=10, pady=10,)
+
+    def click(self, value):
+        self.entry.insert(tk.END, value)
+
+    def clear(self):
+        self.entry.delete(0, tk.END)
+
+class advanced_calculator(calculator):
+
+    def calculate(self):
+        try:
+            result = eval(self.entry.get())
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, result)
+        except:
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, "Error")
+
+    def create_buttons(self):
+
+        buttons = [
+            ['7', '8', '9', '/'],
+            ['4', '5', '6', '*'],
+            ['1', '2', '3', '-'],
+            ['0', '.', '=', '+'],
+            ['C']
+        ]
+
+        for row in buttons:
+            frame = tk.Frame(self.root)
+            frame.pack(expand=True, fill="both")
+
+            for btn in row:
+
+                if btn == "=":
+                    action = self.calculate
+                elif btn == "C":
+                    action = self.clear
+                else:
+                    action = lambda x=btn: self.click(x)
+
+                tk.Button(
+                    frame,
+                    text=btn,
+                    font=("Arial", 18),
+                    command=action
+                ).pack(side="left", expand=True, fill="both")
+
+
 root = tk.Tk()
-root.title("python_calculator")
-root.geometry("400x400")
 
-entry = tk.Entry(root, font=("Arial", 20), justify="right")
-entry.pack(fill="both", padx=10, pady=10, ipady=10)
-
-def click(value):
-    entry.insert(tk.END, value)
-
-def calculate():
-    try:
-        result = eval(entry.get())
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, result)
-    except:
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, "Error")
-
-def clear():
-    entry.delete(0, tk.END)
-
-buttons = [
-    ['7', '8', '9', '/'],
-    ['4', '5', '6', '*'],
-    ['1', '2', '3', '-'],
-    ['0', '.', '=', '+'],
-    ['C']
-]
-
-for row in buttons:
-    frame = tk.Frame(root)
-    frame.pack(expand=True, fill="both")
-
-    for btn in row:
-        if btn == "=":
-            action = calculate
-        elif btn == "C":
-            action = clear
-        else:
-            action = lambda x=btn: click(x)
-
-        tk.Button(
-            frame,
-            text=btn,
-            font=("Arial", 18),
-            command=action
-        ).pack(side="left", expand=True, fill="both")
+calc = advanced_calculator(root)
+calc.create_buttons()
 
 root.mainloop()
