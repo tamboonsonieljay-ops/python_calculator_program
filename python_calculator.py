e0aaeaf2e5a3631b5baa6758715be7ec706fbb6b
Soniel <tamboonsonieljay@gmail.com>
2026-05-08 21:35:26 +0800

    added the function for button clicks

--- python_calculator.py
+++ python_calculator.py
@@ -6,3 +6,6 @@ root.geometry("400x400")
 
 entry = tk.Entry(root, font=("Arial", 20), justify="right")
 entry.pack(fill="both", padx=10, pady=10, ipady=10)
+
+def click(value):
+    entry.insert(tk.END, value)
