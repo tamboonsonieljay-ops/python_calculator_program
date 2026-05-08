ca70cc5fff4fb26afbd81511645d8caded501531
Soniel <tamboonsonieljay@gmail.com>
2026-05-08 21:30:22 +0800

    added an entry box

--- python_calculator.py
+++ python_calculator.py
@@ -4,3 +4,5 @@ root = tk.Tk()
 root.title("python_calculator")
 root.geometry("400x400")
 
+entry = tk.Entry(root, font=("Arial", 20), justify="right")
+entry.pack(fill="both", padx=10, pady=10, ipady=10)
